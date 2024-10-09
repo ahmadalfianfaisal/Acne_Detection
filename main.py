da72fa6ed077ef66e4820169aec6d3d07dd22074
Ahmad Alfian Faisal <154009014+ahmadalfianfaisal@users.noreply.github.com>
2024-10-10 01:49:54 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,22 +1,24 @@
 import streamlit as st
-import cv2
 import numpy as np
-from PIL import Image
+from PIL import Image, ImageDraw
 from ultralytics import YOLO
 
 # Muat model YOLO
 model = YOLO('best.pt')  # Pastikan path ini benar
 
-
 # Fungsi untuk melakukan deteksi jerawat
 def detect_acne(image):
-    # Menggunakan BGR untuk OpenCV
-    img_bgr = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+    # Convert image to NumPy array
+    img_np = np.array(image)
 
-    # Deteksi jerawat
-    results = model.predict(source=img_bgr, imgsz=640)
+    # Deteksi jerawat menggunakan model YOLO
+    results = model.predict(source=img_np, imgsz=640)
 
-    # Menggambar kotak deteksi
+    # Menggambar kotak deteksi di gambar asli
+    img_np_copy = img_np.copy()  # Buat salinan gambar
+    img_pil = Image.fromarray(img_np_copy)
+    draw = ImageDraw.Draw(img_pil)
+
     for result in results:
         boxes = result.boxes
         if boxes is not None and len(boxes.xyxy) > 0:
@@ -25,17 +27,13 @@ def detect_acne(image):
                 conf = boxes.conf[i] if boxes.conf is not None and len(boxes.conf) > i else None
 
                 # Menggambar kotak di gambar asli dengan warna merah
-                cv2.rectangle(img_bgr, (int(x1), int(y1)), (int(x2), int(y2)), color=(0, 0, 255), thickness=2)
+                draw.rectangle([(x1, y1), (x2, y2)], outline="red", width=2)
 
                 if conf is not None:
                     # Menambahkan label dengan confidence score
-                    cv2.putText(img_bgr, f'{conf:.2f}', (int(x1), int(y1) - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5,
-                                (0, 0, 255), 2)
+                    draw.text((x1, y1 - 10), f'{conf:.2f}', fill="red")
 
-    # Mengembalikan gambar ke format RGB
-    img_rgb = cv2.cvtColor(img_bgr, cv2.COLOR_BGR2RGB)
-    return img_rgb
-
+    return img_pil
 
 # Judul aplikasi
 st.title('Deteksi Jerawat Menggunakan YOLOv8')
@@ -47,7 +45,6 @@ uploaded_file = st.file_uploader("Unggah Gambar", type=["jpg", "jpeg", "png"])
 if uploaded_file is not None:
     # Membaca gambar
     image = Image.open(uploaded_file)
-    image_np = np.array(image)
 
     # Tampilkan gambar asli
     st.image(image, caption='Gambar yang Diupload', use_column_width=True)
@@ -55,7 +52,7 @@ if uploaded_file is not None:
     # Tombol untuk melakukan deteksi jerawat
     if st.button('Deteksi Jerawat'):
         # Deteksi jerawat dalam gambar
-        detected_image = detect_acne(image_np)
+        detected_image = detect_acne(image)
 
         # Tampilkan gambar hasil deteksi
         st.image(detected_image, caption='Hasil Deteksi Jerawat', use_column_width=True)
